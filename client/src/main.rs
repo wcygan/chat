51579bece0ba8daef602a64bdc079e63df7b9a86
Will Cygan <wcygan.io@gmail.com>
2023-03-31 23:26:05 -0700

    dial the server from the client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,8 +1,13 @@
+use crate::args::Args;
 use anyhow::Result;
+use clap::Parser;
+use connection::Connection;
 
 mod args;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
+    let args = Args::parse();
+    let conn = Connection::dial(args.address).await?;
     Ok(())
 }
