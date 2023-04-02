c8b40eaf39a8f3626c6eb7d2bde842543d4e5dea
Will Cygan <wcygan.io@gmail.com>
2023-04-01 18:10:42 -0700

    bind to 0.0.0.0

--- server/src/main.rs
+++ server/src/main.rs
@@ -11,7 +11,7 @@ mod server;
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = args::Args::parse();
-    let addr = format!("127.0.0.1:{}", args.port).parse()?;
+    let addr = format!("0.0.0.0:{}", args.port).parse()?;
     println!("listening on {}", addr);
     let shutdown = tokio_utils::ShutdownController::new();
     let (handle, join) = server::spawn_main_loop(&shutdown);
